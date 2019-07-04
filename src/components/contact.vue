e5c08e10fae9df91baab2a0cb11cde6d1ae3d3ec
keisuke ohashi <kog71903@gmail.com>
2019-07-04 14:03:22 +0900

    Finish contact.vue

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <p>ご連絡はkog71903[at]@gmail.comまで</p>
+    <article class="message is-success">
+      <div class="message-body">
+        <p>ご連絡はkog71903[at]gmail.comまで</p>
+      </div>
+    </article>
   </div>
+
+
 </template>
 
 <script>
 </script>
 
 <style lang="sass">
+  article
+    div
+      p
+        font-weight: bold
 </style>
