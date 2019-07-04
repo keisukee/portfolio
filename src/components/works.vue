cc3292c1d426c3ebb492b42bc5dbf977bbfc5dd3
keisuke ohashi <kog71903@gmail.com>
2019-07-04 11:15:32 +0900

    Finish PC-view of works.vue

--- src/components/works.vue
+++ src/components/works.vue
@@ -1,22 +1,44 @@
 <template>
-  <div>
+  <div class="works_container">
+    <h2>Works</h2>
     <ul>
       <li>
-        <div>
-          <dl>
-            <dt>Nukistagram</dt>
-            <dd>エ○サイトです爆</dd>
-          </dl>
-        </div>
+        <div class="box">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image">
+              <a href="https://nukistagram.herokuapp.com">
+                <img src="../assets/nukistagram.png" alt="Nukistagram">
+              </a>
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <h3>
+                <a href="https://nukistagram.herokuapp.com">Nukistagram</a>
+              </h3>
+              <p>
+                Rails×スクレイピングで、海外の大手サイトからエロ動画を収集してくるサイトを作りました。<br>
+                リンクからは本当にエロサイトに飛ぶのでご注意ください。
+              </p>
+            </div>
+            <nav class="level is-mobile">
+              <div class="level-left">
+                <ul>
+                  <li><small>Rails</small></li>
+                  <li><small>Docker</small></li>
+                  <li><small>Nokogiri(スクレイピング)</small></li>
+                  <li><small>Heroku</small></li>
+                  <li><small>CircleCI</small></li>
+                </ul>
+                <br>
+              </div>
+            </nav>
+          </div>
+        </article>
+      </div>
       </li>
-      <li>
-        <div>
-          <dl>
-            <dt>Portfolio</dt>
-            <dd>ワイのポートフォリオ</dd>
-          </dl>
-        </div>
-      </li>
+
     </ul>
   </div>
 </template>
@@ -25,4 +47,32 @@
 </script>
 
 <style lang="sass">
+  .works_container
+    width: 600px
+    padding-top: 20px
+    margin: 0 auto
+    h2
+      font-size: 30px
+      font-weight: bold
+      margin-bottom: 10px
+    ul
+      li
+        .box
+          article
+            .media-left
+              figure
+                img
+                  width: auto
+                  height: 150px
+            .media-content
+              .content
+                p
+              nav
+                ul
+                  display: -webkit-flex
+                  display: -ms-flex
+                  display: flex
+                  li
+                    margin-right: 10px
+
 </style>
